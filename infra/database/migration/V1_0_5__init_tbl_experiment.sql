052eb30df849e87f3224d6f522c0ef9c14caa8ed
Aahil Ashiq Ali <159537206+AahilAshiqAli@users.noreply.github.com>
2026-06-03 10:58:06 +0500

    [EXOPS-3] Add Schema for Projects and Experiments (#6)
    Co-authored-by: Aahil Ashiq Ali <aahilashiqali@SimpliFiPays-MacBook-Pro.local>

--- infra/database/migration/V1_0_5__init_tbl_experiment.sql
+++ infra/database/migration/V1_0_5__init_tbl_experiment.sql
@@ -1,9 +1,11 @@
-CREATE TABLE IF NOT EXISTS tbl_workspace (
+CREATE TABLE IF NOT EXISTS tbl_experiment (
     id                  SERIAL          PRIMARY KEY,
     uuid                VARCHAR(40)     NOT NULL,
-    name                VARCHAR(150)    NOT NULL,
-    email               VARCHAR(150)    NOT NULL,
-    user_uuid           VARCHAR(40),
+    name                VARCHAR(100),
+    description         TEXT,
+    experiment_type     VARCHAR(40)     NOT NUll,
+    project_uuid        VARCHAR(40)     NOT NULL,
+    workspace_uuid      VARCHAR(40)     NOT NULL,
     status              INTEGER         NOT NULL DEFAULT 1,
     creation_date       TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by          VARCHAR(40)     NOT NULL,
