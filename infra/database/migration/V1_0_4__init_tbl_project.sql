f50a12516cf313fbb3983c221b785072bfdd0748
Aahil Ashiq Ali <aahilashiqali@SimpliFiPays-MacBook-Pro.local>
2026-06-03 10:56:13 +0500

    [EXOPS-3] Add Schema for Projects and Experiments

--- infra/database/migration/V1_0_4__init_tbl_project.sql
+++ infra/database/migration/V1_0_4__init_tbl_project.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS tbl_workspace (
+CREATE TABLE IF NOT EXISTS tbl_project (
     id                  SERIAL          PRIMARY KEY,
     uuid                VARCHAR(40)     NOT NULL,
-    name                VARCHAR(150)    NOT NULL,
-    email               VARCHAR(150)    NOT NULL,
-    user_uuid           VARCHAR(40),
+    name                VARCHAR(100)    NOT NULL,
+    description         TEXT,
+    workspace_uuid      VARCHAR(40)     NOT NULL,
     status              INTEGER         NOT NULL DEFAULT 1,
     creation_date       TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by          VARCHAR(40)     NOT NULL,
